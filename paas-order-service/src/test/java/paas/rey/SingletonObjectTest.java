3fb7d6b5d9eea563b5c1991d3b24a51223465a6d
yeyc <215598070@qq.com>
2025-01-17 21:09:38 +0800

    单例模式DEMOTEST

---
+++ paas-order-service/src/test/java/paas/rey/SingletonObjectTest.java
@@ -0,0 +1,12 @@
+package paas.rey;
+
+/**
+ * @Author yeyc
+ * @Description 描述类的作用
+ * @Date 2025/1/17
+ * @Param
+ * @Exception
+ **/
+public class SingletonObjectTest {
+
+}
---
+++ paas-order-service/src/test/java/paas/rey/SingletonPatternDemo.java
@@ -0,0 +1,11 @@
+package paas.rey;
+ /**
+ * @Author yeyc
+ * @Description 描述类的作用
+ * @Date 2025/1/17
+ * @Param 
+ * @Exception
+**/
+public class SingletonPatternDemo {
+
+}
